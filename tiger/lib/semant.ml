914146dad18978cd7c4243b73300aed20ac40323
someonethere <helperrichard15@gmail.com>
2022-05-22 20:15:20 +0300

    more code in semant.ml

--- tiger/lib/semant.ml
+++ tiger/lib/semant.ml
@@ -180,3 +180,189 @@ and trans_expr expr ~env =
 
     let let_expr = Syntax_rewriter.rewrite_for var lo hi body escapes in 
     tr_expr let_expr ~env 
+  
+  and tr_let decs body ~env = 
+    Trace.SemanticAnalysis.tr_let decs body;
+    let (env', exprs) = trans_decs decs ~env in 
+
+    let r = trans_expr body ~env:env' in 
+    ret (Tr.e_let (exprs, r.expr)) r.ty 
+  
+  and tr_record_field rec_typ tfields (name, expr) ~env = 
+    Trace.SemanticAnalysis.tr_record_field name expr rec_typ;
+    match List.Assoc.find tfields ~equal:S.equal name.L.value with 
+    | Some ty_field -> 
+      let r = tr_expr expr ~env in 
+      if T.(ty_field @<> r.ty)
+      then type_mismatch_error3 expr ty_field r.ty 
+      else r.expr
+    | None -> 
+      missing_field_error rec_typ name 
+  
+  and tr_record ty_name vfields ~env = 
+    let open T in 
+    Trace.SemanticAnalysis.tr_record ty_name vfields;
+    let rec_typ = ST.look_typ env.tenv ty_name in 
+    match ~!rec_typ with 
+    | T.Record (tfields, _) -> 
+      let fields = List.map vfields ~f:(tr_record_field rec_typ tfields ~env) in 
+      ret (Tr.e_record fields) rec_typ 
+    | _ -> 
+      type_error ty_name @@ sprintf 
+        "\"%s\" is not a record" (T.to_string rec_typ)
+
+  and tr_array typ size init ~env = 
+      let open T in 
+      Trace.SematicAnalysis.tr_array typ size init;
+      let size_r = tr_expr size ~env in 
+      assert_int size_r.ty;
+      let init_r = tr_expr init ~env in 
+      let arr_ty = ST.look_typ env.tenv typ in 
+      match ~!arr_ty with 
+      | T.Array (tn, _) -> 
+        let t = ~!tn in 
+        let init_ty = ~!(init_r.ty) in 
+        if t = init_ty 
+        then ret (Tr.e_array (size_r.expr, init_r.expr)) arr_ty 
+        else type_mismatch_error4
+          "invalid type of array initial value, " init t init_ty 
+      | _ -> 
+        type_error typ @@ sprintf 
+          "\"%s\" is not array" (T.to_string arr_ty)
+  
+  and tr_var var ~env = 
+      Trace.SemanticAnalysis.tr_var var;
+      match var.L.value with
+      | SimpleVar var -> 
+        tr_simple_var var ~env 
+      | FieldVar (var, field) -> 
+        tr_field_var var field ~env
+      | SubscriptVar (var sub) ->
+        tr_subscript_var var sub ~env
+  
+  and tr_simple_var var ~env = 
+      Trace.SemanticAnalysis.tr_simple_var var;
+      match ST.look_var env.venv var with 
+      | VarEntry ventry -> 
+        ret (Tr.e_simple_var (ventry.access, env.level)) (T.actual ventry.ty)
+      | FunEntry { formals; result; _ } -> 
+        let signature =
+          formals
+          |> List.map ~f:T.to_string 
+          |> String.concat ~sep: ", " in 
+        type_error @@ sprintf 
+          "expected variable, but found a function \"(%s) : %s\""
+          signature (T.to_string result)
+  and tr_field_var var field ~env = 
+      Trace.SemanticAnalysis.tr_field_var var field;
+      let rec_r = tr_var var ~env in 
+      match rec_r.ty with 
+      | Record (fields, _) -> 
+        (match List.findi fields ~f:(fun _ (name, _) -> S.(name = field.L.value)) with 
+        | Some (i, (_, tt)) -> T.(ret (Tr.e_field_var rec_r.expr i) ~!tt)
+        | None -> missing_field_error rec_r.ty field)
+      | _ -> 
+        type_error var @@ sprintf 
+          "expected record, but \"%s\" found"
+          (T.to_string rec_r.ty)
+  
+  and tr_subscript_var var sub ~env = 
+      Trace.SemanticAnalysis.tr_subscript_var var sub;
+      let arr_r = tr_var var ~env in 
+      match arr_r.ty with 
+      | Array(tn, _) -> 
+        let sub_r = tr_expr sub ~env in 
+        assert_int sub_r.ty;
+        T.(ret (Tr.e_subscript_var arr_r.expr sub_r.expr) ~!tn)
+      | _ -> 
+        type_error var @@ sprintf 
+          "\"%s\" is not an array" (T.to_string arr_r.ty)
+  in 
+  tr_expr expr ~env 
+and trans_decs decs ~env = 
+  Trace.SemanticAnalysis.trans_decs decs;
+  List.fold_left decs 
+    ~f: (fun (env, exprs) dec -> 
+         let (env', exprs') = trans_dec dec ~env in
+         (env', exprs @ exprs'))
+    ~init: (env, [])
+
+and trans_dec ~env = function 
+  | TypeDec tys -> (trans_type_decs tys ~env, [])
+  | FunDec fs -> (trans_fun_decs fs ~env, [])
+  | VarDec var -> trans_var_dec var ~env 
+
+and trans_type_decs tys ~env = 
+  let open Syntax in 
+  let tr_ty_head (tns, tenv) ty_dec = 
+    let typ = ty_dec.L.value.type_name in 
+    let tn = ref None in 
+    let tenv' = ST.bind_typ tenv typ (T.Name (typ.L.value, tn)) in 
+    tn :: tns, tenv' in 
+  let (tns, tenv') = List.fold_left tys ~init:([], env.tenv) ~f:tr_ty_head in 
+  let resolve_ty tn ty_dec = 
+    let { typ; _ } = ty_dec.L.value in 
+    let t = trans_ty tenv' typ in 
+    tn := Some t 
+  in 
+  Trace.SemanticAnalysis.trans_type_decs tys;
+  List.iter2_exn (List.rev tns) tys ~f: resolve_ty;
+  { env with tenv = tenv' }
+
+and trans_fun_decs fs ~env = 
+  let open Syntax in 
+  let open Env in 
+
+  let tr_fun_head (sigs, venv) fun_dec = 
+    Trace.SemanticAnalysis.trans_fun_head fun_dec;
+    let { fun_name; params; result_typ; _ } = fun_dec.L.value in 
+    let args = List.map params ~f:(fun p -> p, ST.look_typ env.tenv p.typ) in 
+    let result = match result_typ with 
+      | None -> T.Unit 
+      | Some t -> ST.look_typ env.tenv t in 
+    let esc_formals = List.map params ~f:(fun f -> !(f.escapes)) in 
+    let label = Temp.mk_label None in 
+    let parent = Some env.level in 
+    let level = Tr.new_level ~parent ~label ~formals:esc_formals in 
+    Trace.Translation.new_level level
+    let formals = List.map args ~f: snd in 
+    let entry = FunEntry { level; label; formals; result } in 
+    let venv' = ST.bind_fun venv fun_name entry in 
+    (level, args, result) :: sigs, venv' in 
+  
+  let (sigs, venv') = List.fold_left fs ~f:tr_fun_head ~init:([], env.venv) in 
+
+  let assert_fun_body (level', params, result) fun_dec = 
+    Trace.SemanticAnalysis.assert_fun_body fun_dec result;
+    let add_param e ({ name; escapes; _ }, ty) = 
+      let access = Tr.alloc_local ~level:env.level, ~escapes:!escapes in 
+      Trace.Translation.alloc_local access;
+      let entry = VarEntry { ty; access } in 
+      ST.bind_var e name entry 
+    in 
+    let venv'' = List.fold_left params ~init:venv' ~f:add_param in 
+    let env' = { env with level = level'; venv = venv'' }
+    let { body; _ } = fun_dec.L.value in 
+    let body_ty = trans_expr body ~env:env' in 
+    if T.(body_ty.ty <> result) 
+    then type_mismatch_error4 
+      "type of the body expression doesn't match the declared result type, "
+      body result body_ty.ty;
+    Tr.proc_entry_exit (level', body_ty.expr)
+  in
+  Trace.SemanticAnalysis.trans_fun_decs fs;
+  List.iter2_exn (List.rev sigs) fs ~f: assert_fun_body;
+  { env with venv = venv' }
+  
+  let assert_init var init_ty ~env = 
+    let open Syntax in 
+    let open Env in 
+    let { var_typ; init; _ } = var.L.value in 
+    match var_typ with 
+    | None -> () 
+    | Some ann_ty ->
+      let var_ty = ST.look_typ env.tenv ann_ty in 
+      if T.(var_ty @<> init_ty)
+      then type_mismatch_error3 init var_ty init_ty
+
+    
