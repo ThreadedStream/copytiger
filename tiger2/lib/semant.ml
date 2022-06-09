d023054edeff82c145a807f4111d5e5a50a4e586
gildarov <gildarov@ozon.ru>
2022-06-09 21:47:37 +0300

    copy check_func_dec and tr_dec

--- tiger2/lib/semant.ml
+++ tiger2/lib/semant.ml
@@ -132,3 +132,123 @@ let rec trans_dec (
           | Some t -> U.actual_ty t_env t 
           | None -> 
             Error.Error.error pos ("Type " ^ S.name(name) ^ " can't be resolved!");
+            T.NIL 
+        in 
+        { t_env = S.enter(t_env, name, actual_type); v_env = v_env }
+    in 
+    
+    let check_func_dec (
+        (v_env : venv), 
+        (t_env : tenv),
+        (fs: A.fundec list)) = 
+      let look_type_up (s, p): T.ty = 
+        match S.look(t_env, s) with 
+          | Some t -> t 
+          | None -> (Err.error p ("Type " ^ S.name(s) ^ "has not been declared"); T.NIL)
+      in 
+      
+      let get_type_for_result = function 
+        | Some (s, p) -> look_type_up (s, p)
+        | None -> T.NIL 
+      in 
+
+      let get_type (A.Field {name; escape = _; typ; pos}): T.ty = 
+        look_type_up (typ, pos)
+      in 
+
+      let get_name (A.Field {name}): S.symbol = name in 
+
+      let get_name_type x = {name = get_name x; ty = get_type x} in 
+
+      let check_result_type (expect_type, result_type) = 
+        if T.eq(expect_type, T.NIL) then true 
+        else T.eq(expect_type, result_type)
+      in 
+
+      let add_func_header acc (A.Func {name; params; result; body; pos}) = 
+        let f param = get_type param |> (U.actual_ty t_env) in 
+        let type_list = List.map f params in 
+        let result_type = get_type_for_result result in 
+        let label = Temp.newlabel in 
+        Translate.add_func_header (S.name name) result_type type_list;
+        S.enter(acc, name, E.FunEntry{
+                          formals = type_list;
+                          result = result_type;
+                          label = name;
+                          level = level
+                      });
+        in 
+
+        let add_new_func_entry (cur_v_env: venv) (A.Func {name; params; result; body; pos}) = 
+          let param_name_type = List.map get_name_type params in 
+          let result_type = get_type_for_result result in
+          let escapes = List.map (fun (A.Field {escape}) -> !escape) params in 
+          let label = match S.look(cur_v_env, name) with 
+            | Some(E.FunEntry {label; _}) -> label 
+            | _ -> Temp.newlabel() in 
+          let func_level = Translate.new_level level in 
+
+          let body_type = ref T.NIL in 
+
+          let get_dumb_venv_to_compute_esc () = 
+            let f v_env {name; ty} = 
+              S.enter (v_env, name, E.VarEntry{ ty = ty; access = Translate.dummy_access})
+            in 
+            List.fold_left f cur_v_env param_name_type 
+          in 
+
+          let add_arg_bindings (alloc_addrs: Translate.access list) = 
+            let f v_env {name; ty} access = 
+              S.enter (v_env, name, E.VarEntry{ ty = ty; access = access })
+            in 
+            let body_venv = List.fold_left2 f cur_v_env param_name_type alloc_addrs in 
+            let translate_body () = 
+              let { exp = bodyIr; ty = result_body_type } = trans_exp(body_venv, t_env, func_level, body, break) in 
+              body_type := result_body_type;
+              (result_body_type, bodyIr) 
+            in
+            translate_body
+          in 
+
+          let dumb_v_esc_venv = get_dumb_venv_to_compute_esc() in 
+          let esc_vars = Link.extract_esc(dumb_v_esc_venv, t_env, body) in 
+          let next_esc_order = 1 + List.length esc_vars in 
+
+          let gen_arg_mappings
+              (result, index)
+              (A.Field {escape} as e) = 
+            let esc_order = if !escape then index else -1 in 
+            let new_index = if !escape then index + 1 else index in 
+            let cur_map: arg_name_type_map = {name = get_name e; ty = get_type e; esc_order = esc_order} in 
+            let new_result = result @ [cur_map] in 
+            (new_result, new_index)
+          in 
+          let (args, _) = List.fold_left gen_arg_mappings ([], next_esc_order) params in 
+          Translate.func_dec 
+            func_level
+            (S.name name)
+            result_type
+            esc_vars 
+            args 
+            add_arg_bindings; 
+          (if check_result_type(result_type, !body_type) then ()
+            else 
+              let msg = Printf.sprintf "return type '%s' does not match with '%s'" 
+                        (T.name !body_type) (T.name result_type) in 
+              Err.error pos msg); 
+            cur_v_env
+        in 
+        let base_env = List.fold_left add_func_header v_env fs in 
+        let newv_env = List.fold_left add_new_func_entry base_env fs in 
+        { v_env = newv_env; t_env = t_env }
+      in 
+
+      let tr_dec = function 
+        | (v_env, t_env, (A.VarDec _ as e)) -> check_var_dec (v_env, t_env, e)
+        | (v_env, t_env, A.TypeDec(e)) -> check_type_dec (v_env, t_env, e)
+        | (v_env, t_env, A.FunctionDec(e)) -> check_func_dec (v_env, t_env, e)
+      in 
+      
+      let helper = fun {v_env = v_env; t_env = t_env} dec -> tr_dec (v_env, t_env, dec) in 
+      let result = List.fold_left helper { v_env = v_env; t_env = t_env } exps in 
+      result 
